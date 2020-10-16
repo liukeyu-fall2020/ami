0289fc9d0bd0e3e822f0a3d05fe2a92fcacc5e5f
KeyuLiu-NEU <72110747+KeyuLiu-NEU@users.noreply.github.com>
2020-10-16 13:54:56 -0400

    Update buildAmi.sh

--- buildAmi.sh
+++ buildAmi.sh
@@ -1,6 +1,5 @@
 packer build \
-    -var 'aws_access_key=AKIAQ7IKVIAKAAHFLKWB' \
-    -var 'aws_secret_key=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' \
+
     -var 'aws_region=us-east-1' \
     -var 'subnet_id=subnet-e754e6c6' \
     ami.json
