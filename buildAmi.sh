91edf55e8b48f65efcdf6fd96e15dec5327135fe
KeyuLiu-NEU <72110747+KeyuLiu-NEU@users.noreply.github.com>
2020-10-20 20:41:42 -0400

    Delete buildAmi.sh

--- buildAmi.sh
+++
@@ -1,8 +0,0 @@
-packer build  \
-    -var 'aws_access_key=AKIAQ7IKVIAKB4RAC2G6' \
-    -var 'aws_secret_key=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' \
-    -var 'aws_region=us-east-1'  \
-    -var 'subnet_id=subnet-e754e6c6'  \
-    -var 'ami_users=359410113455' \
-    -var 'source_ami=ami-0817d428a6fb68645'  \
-    ami.json
