cfcb8b6349c658e24149bc94b31d861a6189346a
Keyu Liu <keyuliu@KeyudeMacBook-Pro.local>
2020-11-04 13:24:35 -0500

    assignment5

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 packer build  \
-    -var 'aws_access_key=${{ secrets.AWS_ACCESS_KEY }}' \
-    -var 'aws_secret_key=${{ secrets.AWS_SECRET_KEY }}' \
+    -var 'aws_access_key=AKIAQ7IKVIAKGFOLODVZ' \
+    -var 'aws_secret_key=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' \
     -var 'aws_region=us-east-1'  \
     -var 'subnet_id=subnet-e754e6c6'  \
     -var 'ami_users=359410113455' \
